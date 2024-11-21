acbea28e825f7f0a052377dbcd2ce9000f82c729
AYA <ayafergach70@gmail.com>
2024-11-21 10:06:08 +0100

    Delete programs/nft-staking-v directory

--- programs/nft-staking-v/src/errors.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::error_code;
-
-#[error_code]
-pub enum StakeError {
-    #[msg("Frezze period not passed")]
-    FreezePeriodNotPassed,
-    #[msg("Max stake reached")]
-    MaxStakeReached,
-}
--- programs/nft-staking-v/src/state/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod stake_config;
-pub mod stake_accounts;
-pub mod user_accounts;
-
-pub use stake_config::*;
-pub use stake_accounts::*;

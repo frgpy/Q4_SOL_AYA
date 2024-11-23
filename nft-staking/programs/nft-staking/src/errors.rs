38d8924fee096e71ecdf8cae9c714f2ddc827f7a
frgpy <ayafergach70@gmail.com>
2024-11-23 11:25:25 +0100

    Initial commit: NFT staking

---
+++ nft-staking/programs/nft-staking/src/errors.rs
@@ -0,0 +1,10 @@
+
+use anchor_lang::error_code;
+
+#[error_code]
+pub enum StakeError {
+    #[msg("Frezze period not passed")]
+    FreezePeriodNotPassed,
+    #[msg("Max stake reached")]
+    MaxStakeReached,
+}
